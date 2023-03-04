1e668d409bfaeaedde06f9c9a992f91d78845a27
Lasma Poksane <laaasma.p@gmail.com>
2023-03-04 12:42:34 +0100

    add styles to Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
 const Header = () => {
   return (
     <>
-      <header>
+      <header className={classes.header}>
         <h1>ReactMeals</h1>
         <button>Cart</button>
       </header>
-      <div>
+      <div className={classes["main-image"]}>
         <img src={mealsImage} alt="meals table" />
       </div>
     </>
