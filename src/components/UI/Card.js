2b0828f055efb204a05c01c7b3c1d60841cc492d
Lasma Poksane <laaasma.p@gmail.com>
2023-03-06 10:18:53 +0100

    style Card component

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,5 +1,7 @@
+import classes from "./Card.module.css";
+
 const Card = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className={classes.card}>{children}</div>;
 };
 
 export default Card;
