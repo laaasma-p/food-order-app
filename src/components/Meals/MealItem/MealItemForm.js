14c5840607f9774ed5162119054ca7a4536dffef
Lasma Poksane <laaasma.p@gmail.com>
2023-03-06 23:59:40 +0100

    add styling to MealItemForm

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,6 +1,8 @@
+import classes from "./MealItemForm.module.css";
+
 const MealItemForm = () => {
   return (
-    <form>
+    <form className={classes.form}>
       <input />
       <button>+ Add</button>
     </form>
